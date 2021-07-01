d10f5ede5c0dfe2d8cb4fba9b7178987d701503b
uzielr <uzielroy@gmail.com>
2021-07-01 18:02:31 +0300

    add

---
+++ data/kaggle_data.py
@@ -0,0 +1,117 @@
+"""A modified image folder class
+
+We modify the official PyTorch image folder (https://github.com/pytorch/vision/blob/master/torchvision/datasets/folder.py)
+so that this class can load images from both current directory and its subdirectories.
+"""
+
+import torch.utils.data as data
+import tensorflow as tf
+
+from PIL import Image
+import os
+import os.path
+import glob
+import zipfile
+
+
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
+
+def _parse_image_function(example_proto):
+ return tf.io.parse_single_example(example_proto, train_feature_description)
+
+def make_dataset_kaggle(dir, max_dataset_size=float("inf")):
+    images = []
+    assert os.path.isdir(dir) or os.path.islink(dir), '%s is not a valid directory' % dir
+
+    for root, _, fnames in sorted(os.walk(dir, followlinks=True)):
+        for fname in fnames:
+            if is_image_file(fname):
+                path = os.path.join(root, fname)
+                images.append(path)
+    return images[:min(max_dataset_size, len(images))]
+
+def make_dataset_kaggle(monet=False):
+    train_feature_description = {
+    'class': tf.io.FixedLenFeature([], tf.int64),
+    'id': tf.io.FixedLenFeature([], tf.string),
+    'image': tf.io.FixedLenFeature([], tf.string),
+    }
+    extension = "tfrec.zip"
+    if monet:
+        for item in os.listdir('/content/CUT/kaggle_dataset/'): # loop through items in dir
+            if item.endswith('tfrec.zip') and item.startswith('monet'): # check for "148.tfrec.zip" extension
+                file_name = os.path.abspath(item) # get full path of files
+                zip_ref = zipfile.ZipFile(file_name) # create zipfile object
+                zip_ref.extractall('/content/CUT/kaggle_dataset/monet') # extract file to dir
+                zip_ref.close() # close file
+
+        train_feature_description = {
+            'image': tf.io.FixedLenFeature([], tf.string),
+        }
+        train_files = glob.glob('/content/CUT/kaggle_dataset/monet/*.tfrec')
+        train_ids = []
+        train_class = []
+        train_images = []
+        for i in train_files:
+          train_image_dataset = tf.data.TFRecordDataset(i)
+          train_image_dataset = train_image_dataset.map(_parse_image_function)
+          images = [image_features['image'].numpy() for image_features in train_image_dataset]
+          train_images = train_images + images
+      else:
+        for item in os.listdir('/content/CUT/kaggle_dataset/'): # loop through items in dir
+            if item.endswith('tfrec.zip') and item.startswith('photo'): # check for "148.tfrec.zip" extension
+                file_name = os.path.abspath(item) # get full path of files
+                zip_ref = zipfile.ZipFile(file_name) # create zipfile object
+                zip_ref.extractall('/content/CUT/kaggle_dataset/photo') # extract file to dir
+                zip_ref.close() # close file
+
+        train_feature_description = {
+            'image': tf.io.FixedLenFeature([], tf.string),
+        }
+        train_files = glob.glob('/content/CUT/kaggle_dataset/photo/*.tfrec')
+        train_ids = []
+        train_class = []
+        train_images = []
+        for i in train_files:
+          train_image_dataset = tf.data.TFRecordDataset(i)
+          train_image_dataset = train_image_dataset.map(_parse_image_function)
+          images = [image_features['image'].numpy() for image_features in train_image_dataset]
+          train_images = train_images + images
+
+    return train_files
+
+
+
+
+def default_loader(path):
+    return Image.open(path).convert('RGB')
+
+
+class ImageFolder(data.Dataset):
+
+    def __init__(self, root, transform=None, return_paths=False,
+                 loader=default_loader):
+        imgs = make_dataset(root)
+        if len(imgs) == 0:
+            raise(RuntimeError("Found 0 images in: " + root + "\n"
+                               "Supported image extensions are: " + ",".join(IMG_EXTENSIONS)))
+
+        self.root = root
+        self.imgs = imgs
+        self.transform = transform
+        self.return_paths = return_paths
+        self.loader = loader
+
+    def __getitem__(self, index):
+        path = self.imgs[index]
+        img = self.loader(path)
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.return_paths:
+            return img, path
+        else:
+            return img
+
+    def __len__(self):
+        return len(self.imgs)
--- data/unaligned_dataset.py
+++ data/unaligned_dataset.py
@@ -1,6 +1,7 @@
 import os.path
 from data.base_dataset import BaseDataset, get_transform
 from data.image_folder import make_dataset
+from data.kaggle_data import make_dataset_kaggle
 from PIL import Image
 import random
 import util.util as util
@@ -17,23 +18,20 @@ class UnalignedDataset(BaseDataset):
     '/path/to/data/testA' and '/path/to/data/testB' during test time.
     """
 
-    def __init__(self, opt):
+    def __init__(self, opt,):
         """Initialize this dataset class.
 
         Parameters:
             opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
         """
         BaseDataset.__init__(self, opt)
-        self.dir_A = os.path.join(opt.dataroot, opt.phase + 'A')  # create a path '/path/to/data/trainA'
-        self.dir_B = os.path.join(opt.dataroot, opt.phase + 'B')  # create a path '/path/to/data/trainB'
-
         if opt.phase == "test" and not os.path.exists(self.dir_A) \
            and os.path.exists(os.path.join(opt.dataroot, "valA")):
             self.dir_A = os.path.join(opt.dataroot, "valA")
             self.dir_B = os.path.join(opt.dataroot, "valB")
 
-        self.A_paths = sorted(make_dataset(self.dir_A, opt.max_dataset_size))   # load images from '/path/to/data/trainA'
-        self.B_paths = sorted(make_dataset(self.dir_B, opt.max_dataset_size))    # load images from '/path/to/data/trainB'
+        self.A_paths = sorted(make_dataset_kaggle(True))   # load images from '/path/to/data/trainA'
+        self.B_paths = sorted(make_dataset_kaggle(False))    # load images from '/path/to/data/trainB'
         self.A_size = len(self.A_paths)  # get the size of dataset A
         self.B_size = len(self.B_paths)  # get the size of dataset B
 
@@ -55,8 +53,8 @@ class UnalignedDataset(BaseDataset):
         else:   # randomize the index for domain B to avoid fixed pairs.
             index_B = random.randint(0, self.B_size - 1)
         B_path = self.B_paths[index_B]
-        A_img = Image.open(A_path).convert('RGB')
-        B_img = Image.open(B_path).convert('RGB')
+        A_img = Image.open(io.BytesIO(A_path)).convert('RGB')
+        B_img = Image.open(io.BytesIO(B_path)).convert('RGB')
 
         # Apply image transformation
         # For FastCUT mode, if in finetuning phase (learning rate is decaying),
