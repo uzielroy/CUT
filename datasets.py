586048343d28f4453d1d3b1deddf0141d8306029
uzielr <uzielroy@gmail.com>
2021-07-05 22:55:55 +0300

    update

---
+++ datasets.py
@@ -0,0 +1,108 @@
+import glob
+import random
+import os
+import glob
+import zipfile
+import io
+from torch.utils.data import Dataset
+from PIL import Image
+import torchvision.transforms as transforms
+
+class ImageDataset(Dataset):
+    def __init__(self, root, transforms_=None, unaligned=False, mode='train'):
+        self.transform = transforms.Compose(transforms_)
+        self.unaligned = unaligned
+
+        self.files_A = sorted(glob.glob(os.path.join(root, '%s/A' % mode) + '/*.*'))
+        self.files_B = sorted(glob.glob(os.path.join(root, '%s/B' % mode) + '/*.*'))
+
+    def __getitem__(self, index):
+        item_A = self.transform(Image.open(self.files_A[index % len(self.files_A)]))
+
+        if self.unaligned:
+            item_B = self.transform(Image.open(self.files_B[random.randint(0, len(self.files_B) - 1)]))
+        else:
+            item_B = self.transform(Image.open(self.files_B[index % len(self.files_B)]))
+
+        return {'A': item_A, 'B': item_B}
+
+    def __len__(self):
+        return max(len(self.files_A), len(self.files_B))
+
+
+
+class ImageDataset_kaggle(Dataset):
+    def __init__(self, root, transforms_=None, unaligned=False, mode='train'):
+        self.transform = transforms.Compose(transforms_)
+        self.unaligned = unaligned
+
+        self.files_A = sorted(make_dataset_kaggle(root,False))
+        self.files_B = sorted(glob.glob(os.path.join(root, '%s/B' % mode) + '/*.*'))
+
+    def __getitem__(self, index):
+        item_A = self.transform(io.BytesIO(self.files_A[index % len(self.files_A)]))
+
+        if self.unaligned:
+            item_B = self.transform(io.BytesIO(self.files_B[random.randint(0, len(self.files_B) - 1)]).convert('RGB'))
+        else:
+            item_B = self.transform(io.BytesIO(self.files_B[index % len(self.files_B)]).convert('RGB'))
+
+        return {'A': item_A, 'B': item_B}
+
+    def __len__(self):
+        return max(len(self.files_A), len(self.files_B))
+
+
+def make_dataset_kaggle(path,monet=False):
+    train_feature_description = {
+    'image': tf.io.FixedLenFeature([], tf.string),
+    }
+    extension = "tfrec.zip"
+    if monet:
+        for item in os.listdir(os.path.join(path,'monet'):
+            if item.endswith('tfrec.zip') and item.startswith('monet'):
+                file_name = os.path.abspath(item)
+                zip_ref = zipfile.ZipFile(file_name)
+                zip_ref.extractall(os.path.join(path,'monet'))
+                zip_ref.close() # close file
+
+        train_feature_description = {
+            'image': tf.io.FixedLenFeature([], tf.string),
+        }
+        train_files = glob.glob(os.path.join(path,'monet/*.tfrec'))
+        train_ids = []
+        train_class = []
+        train_images = []
+        for i in train_files:
+          train_image_dataset = tf.data.TFRecordDataset(i)
+          train_image_dataset = train_image_dataset.map(_parse_image_function)
+          images = [image_features['image'].numpy() for image_features in train_image_dataset]
+          train_images = train_images + images
+    else:
+        for item in os.listdir(os.path.join(path,'photo/*.tfrec')): # loop through items in dir
+            if item.endswith('tfrec.zip') and item.startswith('photo'): # check for "148.tfrec.zip" extension
+                file_name = os.path.abspath(item) # get full path of files
+                zip_ref = zipfile.ZipFile(file_name) # create zipfile object
+                zip_ref.extractall(os.path.join(path,'photo/*.tfrec')) # extract file to dir
+                zip_ref.close() # close file
+
+        train_feature_description = {
+            'image': tf.io.FixedLenFeature([], tf.string),
+        }
+        train_files = glob.glob(os.path.join(path,'photo/*.tfrec'))
+        train_ids = []
+        train_class = []
+        train_images = []
+        for i in train_files:
+          train_image_dataset = tf.data.TFRecordDataset(i)
+          train_image_dataset = train_image_dataset.map(_parse_image_function)
+          images = [image_features['image'].numpy() for image_features in train_image_dataset]
+          train_images = train_images + images
+
+    return train_images
+
+
+def _parse_image_function(example_proto):
+ train_feature_description = {'image': tf.io.FixedLenFeature([], tf.string),
+ }
+ return tf.io.parse_single_example(example_proto, train_feature_description)
--- test.py
+++ test.py
@@ -1,70 +1,85 @@
-"""General-purpose test script for image-to-image translation.
+#!/usr/bin/python3
 
-Once you have trained your model with train.py, you can use this script to test the model.
-It will load a saved model from --checkpoints_dir and save the results to --results_dir.
-
-It first creates model and dataset given the option. It will hard-code some parameters.
-It then runs inference for --num_test images and save results to an HTML file.
-
-Example (You need to train models first or download pre-trained models from our website):
-    Test a CycleGAN model (both sides):
-        python test.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan
-
-    Test a CycleGAN model (one side only):
-        python test.py --dataroot datasets/horse2zebra/testA --name horse2zebra_pretrained --model test --no_dropout
-
-    The option '--model test' is used for generating CycleGAN results only for one side.
-    This option will automatically set '--dataset_mode single', which only loads the images from one set.
-    On the contrary, using '--model cycle_gan' requires loading and generating results in both directions,
-    which is sometimes unnecessary. The results will be saved at ./results/.
-    Use '--results_dir <directory_path_to_save_result>' to specify the results directory.
-
-    Test a pix2pix model:
-        python test.py --dataroot ./datasets/facades --name facades_pix2pix --model pix2pix --direction BtoA
-
-See options/base_options.py and options/test_options.py for more test options.
-See training and test tips at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/tips.md
-See frequently asked questions at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/qa.md
-"""
+import argparse
+import sys
 import os
-from options.test_options import TestOptions
-from data import create_dataset
-from models import create_model
-from util.visualizer import save_images
-from util import html
-import util.util as util
 
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+import torch
 
-if __name__ == '__main__':
-    opt = TestOptions().parse()  # get test options
-    # hard-code some parameters for test
-    opt.num_threads = 0   # test code only supports num_threads = 1
-    opt.batch_size = 1    # test code only supports batch_size = 1
-    opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
-    opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
-    opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
-    dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
-    train_dataset = create_dataset(util.copyconf(opt, phase="train"))
-    model = create_model(opt)      # create a model given opt.model and other options
-    # create a webpage for viewing the results
-    web_dir = os.path.join(opt.results_dir, opt.name, '{}_{}'.format(opt.phase, opt.epoch))  # define the website directory
-    print('creating web directory', web_dir)
-    webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.epoch))
+from models import Generator
+from datasets import ImageDataset
 
-    for i, data in enumerate(dataset):
-        if i == 0:
-            model.data_dependent_initialize(data)
-            model.setup(opt)               # regular setup: load and print networks; create schedulers
-            model.parallelize()
-            if opt.eval:
-                model.eval()
-        if i >= opt.num_test:  # only apply our model to opt.num_test images.
-            break
-        model.set_input(data)  # unpack data from data loader
-        model.test()           # run inference
-        visuals = model.get_current_visuals()  # get image results
-        img_path = model.get_image_paths()     # get image paths
-        if i % 5 == 0:  # save images to an HTML file
-            print('processing (%04d)-th image... %s' % (i, img_path))
-        save_images(webpage, visuals, img_path, width=opt.display_winsize)
-    webpage.save()  # save the HTML
+parser = argparse.ArgumentParser()
+parser.add_argument('--batchSize', type=int, default=1, help='size of the batches')
+parser.add_argument('--dataroot', type=str, default='datasets/horse2zebra/', help='root directory of the dataset')
+parser.add_argument('--input_nc', type=int, default=3, help='number of channels of input data')
+parser.add_argument('--output_nc', type=int, default=3, help='number of channels of output data')
+parser.add_argument('--size', type=int, default=256, help='size of the data (squared assumed)')
+parser.add_argument('--cuda', action='store_true', help='use GPU computation')
+parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
+parser.add_argument('--generator_A2B', type=str, default='output/netG_A2B.pth', help='A2B generator checkpoint file')
+parser.add_argument('--generator_B2A', type=str, default='output/netG_B2A.pth', help='B2A generator checkpoint file')
+opt = parser.parse_args()
+print(opt)
+
+if torch.cuda.is_available() and not opt.cuda:
+    print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+
+###### Definition of variables ######
+# Networks
+netG_A2B = Generator(opt.input_nc, opt.output_nc)
+netG_B2A = Generator(opt.output_nc, opt.input_nc)
+
+if opt.cuda:
+    netG_A2B.cuda()
+    netG_B2A.cuda()
+
+# Load state dicts
+netG_A2B.load_state_dict(torch.load(opt.generator_A2B))
+netG_B2A.load_state_dict(torch.load(opt.generator_B2A))
+
+# Set model's test mode
+netG_A2B.eval()
+netG_B2A.eval()
+
+# Inputs & targets memory allocation
+Tensor = torch.cuda.FloatTensor if opt.cuda else torch.Tensor
+input_A = Tensor(opt.batchSize, opt.input_nc, opt.size, opt.size)
+input_B = Tensor(opt.batchSize, opt.output_nc, opt.size, opt.size)
+
+# Dataset loader
+transforms_ = [ transforms.ToTensor(),
+                transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)) ]
+dataloader = DataLoader(ImageDataset(opt.dataroot, transforms_=transforms_, mode='test'), 
+                        batch_size=opt.batchSize, shuffle=False, num_workers=opt.n_cpu)
+###################################
+
+###### Testing######
+
+# Create output dirs if they don't exist
+if not os.path.exists('output/A'):
+    os.makedirs('output/A')
+if not os.path.exists('output/B'):
+    os.makedirs('output/B')
+
+for i, batch in enumerate(dataloader):
+    # Set model input
+    real_A = Variable(input_A.copy_(batch['A']))
+    real_B = Variable(input_B.copy_(batch['B']))
+
+    # Generate output
+    fake_B = 0.5*(netG_A2B(real_A).data + 1.0)
+    fake_A = 0.5*(netG_B2A(real_B).data + 1.0)
+
+    # Save image files
+    save_image(fake_A, 'output/A/%04d.png' % (i+1))
+    save_image(fake_B, 'output/B/%04d.png' % (i+1))
+
+    sys.stdout.write('\rGenerated images %04d of %04d' % (i+1, len(dataloader)))
+
+sys.stdout.write('\n')
+###################################
